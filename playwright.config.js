808b98fade5696310a731b7926836669e1affd69
Arjun M <arjunm0705@gmai.com>
2026-03-05 20:22:43 +0530

    Initial commit - Add Login and Signup tests

--- playwright.config.js
+++ playwright.config.js
@@ -26,15 +26,10 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    headless: false, // show browser
-    viewport: { width: 1280, height: 720 },
+    headless: false,
+    video: 'retain-on-failure',   // 👈 Important
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
